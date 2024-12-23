13b24bf045e9d1a1a089a60fcb7c471486279807
Lucas Marques <lucasses10@gmail.com>
2024-12-23 14:38:23 -0300

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateUsersController } from './controllers/create_users'
+import { envSchema } from './env'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateUsersController],
   providers: [PrismaService],
 })
