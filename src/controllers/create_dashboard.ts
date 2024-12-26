5ffb9924ccc01fd80a07a5993a45a2bbdf58b582
Lucas Marques <lucasses10@gmail.com>
2024-12-26 09:28:10 -0300

    feat: add create dashboard controller

--- src/controllers/create_dashboard.ts
+++ src/controllers/create_dashboard.ts
@@ -1,17 +1,44 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current_user_decorator'
 import { JwtAuthGuard } from 'src/auth/jwt_auth.guard'
 import { UserPayload } from 'src/auth/jwt_strategy'
+import { ZodValidationPipe } from 'src/pipes/zod_validation_pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createDashboardBodySchema = z.object({
+  title: z.string(),
+  link: z.string().url(),
+})
+
+type CreateDashboardBodySchema = z.infer<typeof createDashboardBodySchema>
 
 @Controller('/dashboard')
 @UseGuards(JwtAuthGuard)
 export class CreateDashboardController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user.sub)
+  async handle(
+    @Body(new ZodValidationPipe(createDashboardBodySchema))
+    body: CreateDashboardBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, link } = body
+    const userId = user.sub
 
-    return 'OK'
+    await this.prisma.dahsboard.create({
+      data: {
+        title,
+        link,
+        author: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    })
+
+    return 'Dashboard created successfully.'
   }
 }
