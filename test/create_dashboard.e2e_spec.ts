ce8b5d8a3a495a12f00268063d1c6402b8e6a1b3
Lucas Marques <lucasses10@gmail.com>
2024-12-27 08:08:27 -0300

    test: add create dashboard test

---
+++ test/create_dashboard.e2e_spec.ts
@@ -0,0 +1,55 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Create dashboard (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[POST /dashboard', async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: '123456',
+        document: '12345678901',
+        branch: 'Aurora',
+        role: 'admin',
+      },
+    })
+
+    const accessToken = jwt.sign({ sub: user.id })
+
+    const response = await request(app.getHttpServer())
+      .post('/dashboard')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'Dash test',
+        link: 'http://testdash.com',
+      })
+
+    expect(response.statusCode).toBe(201)
+
+    const dashboardOnDatabase = await prisma.dashboard.findFirst({
+      where: { title: 'Dash test' },
+    })
+
+    expect(dashboardOnDatabase).toBeTruthy()
+  })
+})
